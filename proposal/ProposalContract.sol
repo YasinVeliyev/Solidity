486cea6615fe57e89d706f8dc3888693c40a0f1e
YasinVeliyev <veliyev.yasin@gmail.com>
2024-10-27 10:35:58 +0400

    new file:   proposal/ProposalContract.sol 	new file:   proposal/artifacts/ProposalContract.json 	new file:   proposal/artifacts/ProposalContract_metadata.json 	new file:   proposal/artifacts/build-info/1723b2a788e79c09662c86adbbf5a466.json

---
+++ proposal/ProposalContract.sol
@@ -0,0 +1,20 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ProposalContract {
+    address owner;
+    struct Proposal {
+        string title;
+        string description;
+        uint256 approve;
+        uint256 reject;
+        uint256 pass;
+        uint256 total_vote_to_end;
+        bool current_state;
+        bool is_active;
+    }
+
+    mapping(uint256 => Proposal) proposal_history;
+
+}
